d64574d91d2f5b36e5e8d402967c9c201ae63959
Sangil <ronniejohnmarksangil@gmail.com>
2024-04-17 17:52:08 +0800

    Add project files.

---
+++ JSON/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace JSON
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(192, 42);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(304, 22);
+            this.txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(192, 88);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(304, 22);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(214, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SIGN UP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(362, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(214, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "LOGIN";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // dgvData
+            // 
+            this.dgvData.AllowUserToAddRows = false;
+            this.dgvData.AllowUserToDeleteRows = false;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvData.Location = new System.Drawing.Point(699, 54);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.ReadOnly = true;
+            this.dgvData.RowHeadersWidth = 51;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.Size = new System.Drawing.Size(356, 279);
+            this.dgvData.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(773, 356);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(214, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "GENERATE USER";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 404);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dgvData);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ JSON/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+namespace JSON
+{
+    public partial class Form1 : Form
+    {
+        public string file_name = "user.json";
+        List<dynamic> userData = new List<dynamic>();
+        
+        public Form1()
+        {
+            InitializeComponent();
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            var credentials = new
+            {
+                username = txtUsername.Text,
+                password = txtPassword.Text,
+            };
+
+     
+            userData.Add(credentials);
+            var json = JsonConvert.SerializeObject(userData);
+            File.WriteAllText(file_name, json);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            var jsons = File.ReadAllText(file_name);
+            userData = JsonConvert.DeserializeObject<List<dynamic>>(jsons);
+
+            foreach (var credentials in userData)
+            {
+                string username = credentials["username"];
+                string password = credentials["password"];
+
+                if (txtUsername.Text == username && txtPassword.Text == password)
+                {
+                    MessageBox.Show("Login!");
+                    return;
+                } else
+                {
+                   
+                }
+              
+            }
+            MessageBox.Show("Invalid Username or Password");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var jsons = File.ReadAllText(file_name);
+            userData = JsonConvert.DeserializeObject<List<dynamic>>(jsons);
+
+            dgvData.DataSource = userData;
+        }
+    }
+}
